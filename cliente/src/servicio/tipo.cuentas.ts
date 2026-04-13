0507341998f1e97e11b4e673c02d23cbaf6beddd
josesion <josesion1388@gmail.com>
2026-04-13 14:06:58 -0300

    Servicio fetch listo con su archivo de tipado

---
+++ cliente/src/servicio/tipo.cuentas.ts
@@ -0,0 +1,114 @@
+import { PAGINA } from "./variables.globales";
+import { apiFetch ,type ApiResponse  } from "../utils/apiFetch";
+import { verificarAutenticacion } from "../hooks/verificacionUsuario";
+
+
+import type * as TipadoTipoCuentas from "../tipadosTs/tipo.cuents";
+
+
+export const altaTipoCuenta = async( data : TipadoTipoCuentas.TipoCuentas) 
+: Promise<ApiResponse<TipadoTipoCuentas.ResultTipoCuentasAlta>> => {
+   
+    const verificarUser= await verificarAutenticacion();
+    if (verificarUser.autenticado === false) {
+        return {
+            error: true,
+            message: "Usuario no autenticado",
+            statusCode: 401,
+            code: "NOT_AUTHENTICATED",
+            errorsDetails: undefined
+        };
+    };
+
+   const ruta =  `${PAGINA}api/alta_cuenta`;   
+
+    return await apiFetch (  ruta , {
+        method : "POST",
+        body : {
+            id_escuela : data.id_escuela,
+            nombre_cuenta : data.nombre_cuenta,
+            tipo_cuenta : data.tipo_cuenta
+        }
+    });
+
+};
+
+export const modTipoCuenta = async( data : TipadoTipoCuentas.TipoCuentasMod) 
+: Promise<ApiResponse<TipadoTipoCuentas.ResultTipoCuentaMod>>=> {
+  
+    const verificarUser= await verificarAutenticacion();
+    if (verificarUser.autenticado === false) {
+        return {
+            error: true,
+            message: "Usuario no autenticado",
+            statusCode: 401,
+            code: "NOT_AUTHENTICATED",
+            errorsDetails: undefined
+        };
+    };
+    
+    const rutaCompleta = `${PAGINA}api/mod_cuenta/${data.id_cuenta}/${data.id_escuela}`;     
+
+    return await apiFetch( rutaCompleta, {
+        method : "PUT",
+        body : {
+              nuevo_nombre_cuenta  : data.nuevo_nombre_cuenta,
+              nuevo_tipo_cuenta : data.nuevo_tipo_cuenta
+        }
+    });
+
+};
+
+export const estadoTipoCuenta = async ( data : TipadoTipoCuentas.EstadoTipoCuenta)
+: Promise<ApiResponse<TipadoTipoCuentas.ResultEstadoTipoCuenta>> => {
+    
+    const verificarUser= await verificarAutenticacion();
+    if (verificarUser.autenticado === false) {
+        return {
+            error: true,
+            message: "Usuario no autenticado",
+            statusCode: 401,
+            code: "NOT_AUTHENTICATED",
+            errorsDetails: undefined
+        };
+    }; 
+    
+    const rutaCompleta = `${PAGINA}api/estado_cuenta/${data.id_cuenta}/${data.id_escuela}/${data.estado}`;  
+
+    return await apiFetch( rutaCompleta, {
+        method : "PUT"
+    });
+};
+
+export const listaTipoCuentas = async ( data : TipadoTipoCuentas.ListaTipoCuentas , signal?: AbortSignal)
+: Promise<ApiResponse<TipadoTipoCuentas.ResultListaTipoCuentas[]>> => {
+   
+    const verificarUser= await verificarAutenticacion();
+    if (verificarUser.autenticado === false) {
+        return {
+            error: true,
+            message: "Usuario no autenticado",
+            statusCode: 401,
+            code: "NOT_AUTHENTICATED",
+            errorsDetails: undefined
+        };
+    };
+    
+    const parametrosConvertidos = {
+
+        nombre_cuenta : data.nombre_cuenta,
+        tipo_cuenta : data.tipo_cuenta,
+        estado : data.estado,
+        
+        id_escuela : data.id_escuela.toString(),
+        limit : data.limit.toString(),
+        pagina : data.pagina.toString()
+    };
+
+   const rutaCompleta = `${PAGINA}api/list_tipos_cuentas?${new URLSearchParams(parametrosConvertidos).toString()}`;  
+
+   return await apiFetch( rutaCompleta, {
+        method : "GET",
+        signal : signal 
+   });
+};
---
+++ cliente/src/tipadosTs/tipo.cuents.ts
@@ -0,0 +1,55 @@
+
+type tipo_cuenta =  "fisico" | "virtual" | "todos" ;
+type estado_cuenta = "activos" | "inactivos" | "todos";
+
+export interface TipoCuentas {
+    id_escuela : number,
+    nombre_cuenta : string,
+    tipo_cuenta : tipo_cuenta,
+}; 
+
+export interface ResultTipoCuentasAlta {
+    id: number,
+    nombre_cuenta : string,
+    tipo_cuenta : tipo_cuenta    
+};
+
+export interface TipoCuentasMod {
+   nuevo_nombre_cuenta : string,
+   nuevo_tipo_cuenta : tipo_cuenta,
+   id_cuenta : number,
+   id_escuela : number 
+};
+
+export interface ResultTipoCuentaMod {
+    id_cuenta : number,
+    nuevo_nombre_cuenta : string,
+    nuevo_tipo_cuenta : tipo_cuenta,   
+};
+
+export interface EstadoTipoCuenta {
+    id_cuenta : number,
+    id_escuela : number,
+    estado : estado_cuenta     
+};
+
+export interface ResultEstadoTipoCuenta {
+    id_cuenta : number,
+    estado : estado_cuenta
+};
+
+export interface ListaTipoCuentas {
+    nombre_cuenta : string,
+    tipo_cuenta : tipo_cuenta,
+    estado : estado_cuenta,
+    
+    id_escuela : number,
+    limit : number,
+    pagina : number
+};
+
+export interface ResultListaTipoCuentas {
+    id_cuenta : number,
+    nombre_cuenta : string,
+    tipo_cuenta : tipo_cuenta
+};
