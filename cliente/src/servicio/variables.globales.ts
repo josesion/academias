40f979ea4592855eefd05be78a4120a3e088a73c
josesion <josesion1388@gmail.com>
2026-08-22 16:26:41 -0300

    modifique la variable pagina

--- cliente/src/servicio/variables.globales.ts
+++ cliente/src/servicio/variables.globales.ts
@@ -1,7 +1,4 @@
-import dotenv from "dotenv";
 
-dotenv.config();
-
-export const PAGINA = import.meta.env.VITE_API_URL || process.env.PAGI;
+export const PAGINA = import.meta.env.VITE_API_URL;
 //export const PAGINA = "http://localhost:4000/";
 
